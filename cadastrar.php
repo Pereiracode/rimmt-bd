67a2fd7f124271cb60b9e81e341e71ee564adab4
rafael-conrado <rafael070893@hotmail.com>
2020-06-26 22:05:44 -0300

    mascaras

--- cadastrar.php
+++ cadastrar.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet"  href="css/estilo.css">
+	
 	<title>Projeto Login</title>
 </head>
 <body>
@@ -10,10 +11,12 @@
 	<h1>Cadastre-se</h1>
 	<form method="POST" action="_inserir_usuario.php">
 		<input type="text" name="nome" placeholder="Nome Completo" maxlength="45">
-		<input type="text" name="telefone" placeholder="Telefone" maxlength="30">
-		<input type="email" name="email" placeholder="Usuario" maxlength="40">
+		<input type="text" data-js="phone" name="celular" placeholder="Celular (xxx) xxxxx-xxxxx" maxlength="30">
+		<input type="text" data-js="cpf" name="cpf"  placeholder="CPF (xxx.xxx.xxx-xx)">
+		<input type="text"  name="Endereco" placeholder="Endereço" maxlength="100">
+		<input type="email" name="email" placeholder="E-mail" maxlength="40">
 		<input type="password" name="senha" id="txtSenha" placeholder="Senha" maxlength="20">
-		<input type="password" name="confSenha" placeholder="Confirmar senha" oninput="validaSenha(this)">
+		<input type="password" name="confSenha" placeholder="Confirmar senha" oninput="validaSenha(this)"  >
 		<input type="submit" value="CADASTRAR" class="entrar">
 	</form>
 	<a href="index.php">Já tem Cadastro? <strong>Faça login!</strong></a>
@@ -21,14 +24,6 @@
 
 
 
-<script>
-	function validaSenha (input){ 
-	if (input.value != document.getElementById('txtSenha').value) {
-    input.setCustomValidity('Repita a senha corretamente');
-  } else {
-    input.setCustomValidity('');
-  }
-}
-</script>
+<script src="./js/script.js"></script>
 </body>
 </html>
